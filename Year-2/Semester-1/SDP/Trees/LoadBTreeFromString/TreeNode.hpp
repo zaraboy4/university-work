a4d5977c2390c98678f2d8c7f708f8606b720343
Yoan <165042472+zaraboy4@users.noreply.github.com>
2025-02-04 23:45:13 +0200

    remove function added

--- Year-2/Semester-1/SDP/Trees/LoadBTreeFromString/TreeNode.hpp
+++ Year-2/Semester-1/SDP/Trees/LoadBTreeFromString/TreeNode.hpp
@@ -23,3 +23,30 @@ void print(Node* root) {
     std::cout << root->data << ' ';
     print(root->right);
 }
+
+Node* extractMin(Node*& root) {
+    if (root->left) {
+        return extractMin(root->left);
+    }
+    Node* n = root;
+    root = root->right;
+    return n;
+}
+
+bool remove(Node*& root, const int& el) {
+    if (!root) return false;
+    if (root->data == el) {
+        Node* detach = root;
+        if (!root->left) root = root->right;
+        else if (!root->right) root = root->left;
+        else {
+            Node* n = extractMin(root->right);
+            n->left = root->left;
+            n->right = root->right;
+            root = n;
+        }
+        delete detach;
+        return true;
+    }
+    return remove(el < root->data ? root->left : root->right, el);
+}
